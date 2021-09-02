7149d838eff1947e99122d71472294349cf97153
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2021-09-02 13:58:25 +0530

    Calculator Completed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const welcomeTypes = ["Good Morning", "Good Afternoon", "Good Evening"];
 let welcomeText = "";
 
 if (hour < 12) welcomeText = welcomeTypes[0];
-else if (hour < 18) welcomeText = welcomeTypes[1];
+else if (hour < 16) welcomeText = welcomeTypes[1];
 else welcomeText = welcomeTypes[2];
 
 greeting.innerHTML = welcomeText + ", Let's get things done.";
